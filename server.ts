7b9e3075e22c4ef335e5a62e21c79b607eaba041
dnyanesh75 <dnyaneshlohar75@gmail.com>
2025-06-23 19:25:44 +0530

    deploy

--- server.ts
+++ server.ts
@@ -2,7 +2,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 dotenv.config();
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 import express from 'express';
 import db from "./config/db";
